8ef4f433daa1635b449a0a8a4bd498ae93552b33
Brady Isom <bradyisom@gmail.com>
2016-10-14 21:59:02 -0600

    intial commit with Firebase and Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'SP90X';
+  email: string;
+  password: string;
+  list: FirebaseListObservable<any>;
+  user: FirebaseObjectObservable<any>;
+
+  constructor(private af:AngularFire) {
+    this.af.auth.subscribe(auth => {
+      console.log(auth);
+      if (auth) {
+        this.list = af.database.list(`/schedules/${auth.uid}`);
+        this.user = af.database.object(`/users/${auth.uid}`);       
+      }
+      else {
+        this.list = null;
+        this.user = null;
+      }
+    });
+  }
+
+  login () {
+    this.af.auth.login({
+      email: this.email,
+      password: this.password
+    });
+  }
+
+  logout() {
+    this.af.auth.logout();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+import { Gravatar } from 'ng2-gravatar-directive';
 
 import { AppComponent } from './app.component';
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyDX5ot8wh4i9EXP4Tpx_3Y8SU3o6S1dIAo",
+  authDomain: "sp90x.firebaseapp.com",
+  databaseURL: "https://sp90x.firebaseio.com",
+  storageBucket: "project-8976456987898776126.appspot.com",
+  messagingSenderId: "812558087097"
+};
+
+export const firebaseAuthConfig = {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Password
+};
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Gravatar
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
